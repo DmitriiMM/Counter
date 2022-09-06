d08dc930c87cd32102d643efa04f3f3c8ac9efe8
DmitriiMM <martinovit72@gmail.com>
2022-09-06 22:43:04 +0500

    Work on mistakes after review. Chang the access modifier of a property and remove viewDidLoad()

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,13 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var count: Int = 0
+    private var count: Int = 0
     @IBOutlet weak var countLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     @IBAction func buttonTapped(_ sender: Any) {
         count += 1
         countLabel.text = "Значение счетчика: \(count)"
